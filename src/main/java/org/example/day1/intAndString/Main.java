c45877e74825923859be5d0beee9acf702472060
kimssin1991 <kimsssin1991@naver.com>
2026-07-09 14:23:46 +0900

    main추가

---
+++ src/main/java/org/example/day1/intAndString/Main.java
@@ -0,0 +1,9 @@
+package org.example.day1.intAndString;
+
+public class Main {
+    static void main() {
+        System.out.println("제이름은" + "김신영");
+        System.out.println("제나이는" + "36");
+
+    }
+}
